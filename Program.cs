53b74250644466d62a1210db3c613a4ebdce4fbe
“teji1998” <tejasvinirpk@gmail.com>
2020-11-23 01:05:30 +0530

    Added:Code to check if employee is present or not

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace employeeWageProblem
     {
         static void Main(string[] args)
         {
+            const int IS_PRESENT = 1;
             Console.WriteLine("Welcome to employee wage computation problem!");
+            Random random = new Random();
+            int attendance = random.Next(0, 2);
+            if (attendance == IS_PRESENT)
+            {
+                Console.WriteLine("The employee is present!");
+            }
+            else
+            {
+                Console.WriteLine("The employee is not present");
+            }
+            
+
+
         }
     }
 }
